93076c6d43ee92ba356d621a741130348d8646ad
rick-kenti <erickmakenti@gmail.com>
2025-09-20 14:24:32 +0300

    building loop

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// Function that returns an array of thank you messages
+function writeCards(names, event) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouMessages;
+}
+
+// Function that counts down from a given number and logs each number
+function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+        console.log(i);
+    }
+}
